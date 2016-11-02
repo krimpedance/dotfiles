a8a2a28b3aae4fd18dfe576e3372f135f094f507
krimpedance <krimpedance@gmail.com>
2016-11-02 13:40:25 +0900

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,17 +1,48 @@
 #!/bin/sh
 
+function errorHandling() {
+  echo 'Error...'
+  exit 1;
+}
+
+
 # link
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
+echo 'links has been completed!'
 
 # Install NeoBundle
+echo 'NeoBundle: installing ...'
 if [ ! -d ~/.vim/bundle ]; then
   mkdir -p ~/.vim/bundle
 fi
 
 if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
   echo 'clone'
-  git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+  git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim || errorHandling
 fi
+echo 'NeoBundle: installed!'
+
+# Bash extension for Git
+TLFrameworkURL=`grep "# For Git" ~/.bashrc`
+if [ -z "$TLFrameworkURL" ]; then
+  echo 'Install bash extension for Git...'
+  mkdir -p /usr/local/etc/bash_completion.d/
+  curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh || errorHandling
+  curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash || errorHandling
+  mv git-prompt.sh /usr/local/etc/bash_completion.d/.
+  mv git-completion.bash /usr/local/etc/bash_completion.d/.
+
+  echo '' >> $HOME/.bashrc
+  echo '# For Git' >> $HOME/.bashrc
+  echo 'source /usr/local/etc/bash_completion.d/git-completion.bash' >> $HOME/.bashrc
+  echo 'source /usr/local/etc/bash_completion.d/git-prompt.sh' >> $HOME/.bashrc
+  echo 'GIT_PS1_SHOWDIRTYSTATE=true' >> $HOME/.bashrc
+  echo "export PS1='\h\[\033[00m\]:\W\[\033[31m\]$(__git_ps1 [%s])\[\033[00m\]\$ '" >> $HOME/.bashrc
+fi
+
+source $HOME/".bashrc"
+
+echo 'Completed!🍺'
